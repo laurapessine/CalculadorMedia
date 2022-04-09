d5c0d9207862af7bb2b06edd6625a9198ebd7ad7
laurapessine <laurapessine@outlook.com>
2022-04-09 16:28:52 -0300

    .

--- app/src/main/java/pacote/aula08/MainActivity.java
+++ app/src/main/java/pacote/aula08/MainActivity.java
@@ -1,13 +1,13 @@
 package pacote.aula08;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity {
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnExibir;
     private Button btnSair;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,37 +33,35 @@ public class MainActivity extends AppCompatActivity {
         btnExibir = findViewById(R.id.btnExibir);
         btnSair = findViewById(R.id.btnSair);
         tvValorMedia = findViewById(R.id.tvValorMedia);
-        btnSair.setOnClickListener(new ListenerBtnSair());
-        btnExibir.setOnClickListener(new ListenerBtnExibir());
-        btnCalcular.setOnClickListener(new ListenerBtnCalcular());
-    }
-
-    class ListenerBtnSair implements View.OnClickListener {
-        @Override
-        public void onClick(View view) {
-            System.exit(0);
-        }
-    }
-
-    class ListenerBtnExibir implements View.OnClickListener {
-        @Override
-        public void onClick(View view) {
+        btnSair.setOnClickListener(view -> System.exit(0));
+        /* >>> lambda (linha acima ^) substitui a forma abaixo:
+        btnSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.exit(0);
+            }
+        });
+        >>> economiza linhas e economiza imports (✿◡‿◡)!*/
+        btnExibir.setOnClickListener(view -> {
             String msg = "";
             msg += txtNome.getText() + "\n";
             msg += txtEmail.getText() + "\n";
             msg += "Notas: " + txtNota1.getText() + " e " + txtNota2.getText();
             Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
-        }
-    }
-
-    class ListenerBtnCalcular implements View.OnClickListener {
-        @Override
-        public void onClick(View view) {
+        });
+        btnCalcular.setOnClickListener(view -> {
             double n1, n2, media;
             n1 = Double.parseDouble(txtNota1.getText().toString());
             n2 = Double.parseDouble(txtNota2.getText().toString());
             media = (n1 + n2) / 2;
             tvValorMedia.setText(Double.toString(media));
-        }
+        });
     }
+    /* >>> forma mais antiga (classe interna não-anônima) ¬_¬:
+    class ListenerBtnSair implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            System.exit(0);
+        }
+    }*/
 }
